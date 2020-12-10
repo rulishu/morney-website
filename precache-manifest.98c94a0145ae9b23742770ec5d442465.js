1b9c410dea9134d1de1d9d65c14fb65db8f8a142
xiaoshu <1324214367@qq.com>
2020-12-10 09:51:36 +0800

    deploy

---
+++ precache-manifest.98c94a0145ae9b23742770ec5d442465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81e4bdf168dd047319d",
+    "url": "/morney-website/css/app.3f67579f.css"
+  },
+  {
+    "revision": "724de569ee4f48d5aa15e9c386a0dd13",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "c81e4bdf168dd047319d",
+    "url": "/morney-website/js/app.b3af266c.js"
+  },
+  {
+    "revision": "60d20fc3f7e5e2670df0",
+    "url": "/morney-website/js/chunk-vendors.b976ccef.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "6f55462b65fca352c69857719e1a3087",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
